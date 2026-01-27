e9731f8dac385075fda2fe36197c8298267a2e55
Mohit Kadekar <mohitkadekar7@gmail.com>
2026-01-27 13:29:40 +0530

    Login Page Created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<div>Login Page </div>} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
